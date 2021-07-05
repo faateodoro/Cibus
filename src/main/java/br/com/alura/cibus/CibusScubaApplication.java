b4d7ca2e1e553aeeec427800f1e7b556249580a1
Fabiano Teodoro <faateodoro@gmail.com>
2021-07-05 16:11:09 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/cibus/CibusScubaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.cibus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CibusScubaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CibusScubaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/cibus/CibusScubaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.cibus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CibusScubaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
